05b194ff75e1af915063acfd7ea8ccab620d770a
tjksiazek <tjksiazek@gmail.com>
2020-02-03 11:35:38 -0600

    Add files via upload

---
+++ Celcius-to-Fahrenheit.py
@@ -0,0 +1,15 @@
+temp = input("Enter Temp in Celsius")
+Celsius = float (temp)
+Fahrenheit = 1.8 * Celsius + 32
+
+print ("Temperature:", Celsius, "Celsius = ", Fahrenheit, " Fahrenheit")
+
+
+
+
+#####
+temp = input("Enter Temp in Fahrenheit")
+Fahrenheit = float (temp)
+
+Celsius = (Fahrenheit - 32) * 5/9
+print ("Temperature:" , Fahrenheit, "Fahrenheit = ", Celsius, "Celsius")
